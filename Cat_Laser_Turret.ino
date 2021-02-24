32978b23a857d5617ad80658867d0a52e007d503
Chan Davis <chandlerfdavis@gmail.com>
2021-02-23 20:05:44 -0600

    Add keybindings to controller script and clean up arduino file
    Added the following keybindings:
       * escape - Stops mouse/keyboard listeners, closes the serial port, and exits the script
       * b - Outputs the number of bytes in the RX and TX buffers
       * f - Outputs all bytes currently present in the RX buffer
       * t - Toggles printing of mouse coordinates on mouse move
       * space - sends the mouse coordinates of the next mouse move to the TX buffer
    
    Also, cleaned up the commented-out code in the .ino file

--- Cat_Laser_Turret.ino
+++ Cat_Laser_Turret.ino
@@ -4,6 +4,7 @@ Servo x_servo;
 Servo y_servo;
 bool string_ready;
 String serial_str = "";
+char char_arr[2];
 
 int pos = 0;
 
@@ -14,30 +15,15 @@ void setup() {
    pinMode(5, OUTPUT);
 
    Serial.begin(9600); /* Set the bod rate. */
-   Serial.print("Serial connection established.");
+   Serial.println("Serial connection established.");
 
    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB
+      // wait for serial port to connect. Needed for native USB
    }
 
 }
 
 void loop() {
-   // // put your main code here, to run repeatedly:
-   // digitalWrite(5, HIGH);
-   // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-   //    // in steps of 1 degree
-   //    x_servo.write(pos); // tell servo to go to position in variable 'pos'
-   //    y_servo.write(pos); // tell servo to go to position in variable 'pos'
-   //    delay(15); // waits 15ms for the servo to reach the position
-   // }
-
-   // digitalWrite(5, LOW);
-   // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-   //    x_servo.write(pos); // tell servo to go to position in variable 'pos'
-   //    y_servo.write(pos); // tell servo to go to position in variable 'pos'
-   //    delay(15); // waits 15ms for the servo to reach the position
-   // }
 
    if (string_ready) {
       Serial.println(serial_str);
@@ -61,52 +47,21 @@ void loop() {
 void serialEvent() {
    int is_int = false;
    int y_index = -1;
-   int y = 0;
+   
    int x = 0;
+   int y = 0;
+
    int rot_x = 0;
    int rot_y = 0;
 
+   int read_byte = 0; /* The signed byte read from the RX buffer. */
 
+   /* While data is present in the RX buffer, transfer it to the TX buffer byte by byte. */
    while (Serial.available()) {
+      read_byte = Serial.read();
 
-      // get the new byte:
-      char charmeleon = (char) Serial.read();
-
-
-      // add it to the inputString:
-      serial_str += charmeleon;
-
-      // if the incoming character is a newline, set a flag so the main loop can
-      // do something about it:
-      if (charmeleon == '\n') {
-         string_ready = true;
-      }
+      Serial.println(read_byte, DEC);
    }
-
-
-   if (!Serial.available())
-   {
-      /* Parse 'X<int>Y<int>'. Each <int> is three digits. */
-      if (serial_str.startsWith("$")) {
-         y_index = serial_str.indexOf("Y");
-         
-         if (y_index > 0)
-         {
-            
-            x = serial_str.substring(2, y_index).toInt();
-            y = serial_str.substring(y_index+1).toInt();
-
-            x_servo.write(x);
-            y_servo.write(y);x
-            
-            Serial.print("X:");
-            Serial.print(x);
-            Serial.print(", Y:");
-            Serial.println(y);
-         }
-      }
-   }
-
 }
 
 /**
--- controller.py
+++ controller.py
@@ -1,49 +1,78 @@
-""" Pynput docs: https://pynput.readthedocs.io/en/latest/ """
 from pynput import mouse, keyboard
 import serial
 
 
-# The 'with' keyword is cool! https://www.geeksforgeeks.org/with-statement-in-python/
+# Note to self: the 'with' keyword is cool! https://www.geeksforgeeks.org/with-statement-in-python/
 
 """
 TODO: 
    Add error checking
+   Add serial monitoring (print output sent from device)
 """
+# Output that the controller script is started.
+print('Laser turret controller started.')
 
-ser = serial.Serial('COM5', baudrate=9600)  # open serial port
-print(ser.name)         # check which port was really used
+ser = serial.Serial('COM5', baudrate=9600, timeout=1)  # open serial port
+print(f'Communicating through serial port {ser.name}')         # check which port was really used
 
+send_coords  = False # Should mouse coordinates be sent to the arduino?
+print_coords = False # Should mouse coordinates be output on move?
 
-# TODO: Add a key listener and add a kill hotkey
 def on_press(key):
    try:
       print(f'key pressed {key}')
-      if key == keyboard.Key.tab:
-         return False
    except AttributeError:
       print(f'special key pressed {key}')
 
 def on_release(key):
+   global send_coords
+   global print_coords
+
+   # If escape key is hit, stop listeners, close serial ports, and end this program.
+   if key == keyboard.Key.esc:
+      mouse_listener.stop() # Stop the mouse listener.
+      ser.close() # Close the serial port.
+      return False # Return false to end the key listener loop, which ends the program.
+
+   # Get input and output buffer information.
+   if key == keyboard.KeyCode().from_char('b'):
+      print(f'in-buffer bytes: {ser.in_waiting}\nout-buffer bytes: {ser.out_waiting}')
+
+   # Print all bytes in the input buffer, if any.
+   if key == keyboard.KeyCode().from_char('f'):
+      if ser.in_waiting > 0:
+         print(ser.read(ser.in_waiting))
+
+   # Place mouse coordinates (currently, just X) in the output buffer.
    if key == keyboard.Key.space:
-      mouse_listener.stop()
-      ser.close()             # close port
-      return False
+      send_coords = True
+
+   # Toggle output of mouse coordinates to console window.
+   if key == keyboard.KeyCode.from_char('t'):
+      print_coords = not print_coords
+      
 
 def on_move(x, y):
-   print(ser.read())
-   
+   global send_coords
+   global print_coords
 
-   # ser.write(b'$X')     # write a string
-   ser.write(x + y)
+   if print_coords:
+      print(f'{x}, {y}')
 
-def on_click(x):
-   print(f"clicked {f}")
+   if send_coords:
+      print(f'sending: {x}, {y}')
+      ser.write(x)
+      send_coords = False
+
+def on_click(x, y, button, pressed):
+   pass
 
 # Create a non-blocking mouse listener and start it.
 mouse_listener = mouse.Listener(on_move=on_move, on_click=on_click)
 mouse_listener.start()
 
 # Run a loop for a key listener
-with keyboard.Listener(on_press=on_press, on_release=on_release) as key_listener:
+with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as key_listener:
    key_listener.join()
 
+print('Laser turret controller ended.')
