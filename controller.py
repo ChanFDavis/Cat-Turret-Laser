4e07eb74fe6ee46524eefbdea676ba0ef37cc7c2
Chan Davis <cfdavis@iastate.edu>
2021-03-13 17:12:06 -0600

    Fix encoding of coordinates in controller script using struct.pack()
    - Added importing of the 'struct' module, so that I can use the
    struct.pack() function, which is what I was looking for instead of
    bytes().
    - Added global variables to hold the maximum X and Y dimensions of the
    monitors used during development so far.
    - Added helper function 'mouse_coord_to_servo_angle()' to translate a
    given coordinate's place on a scale between zero and a given maximum
    value to an angle between 0 and 180.
    	+ This is used on both the X and Y mouse coordinates before sending the translated angles to the arduino.
    - Added packing of controller data into a byte array of size 2, where the first byte holds the X-axis angle data and the second byte holds the Y-Axis angle data.
    - Added some debugging output in the controller script.
    	+ I've disabled some of these until I decide to remove them or add some script argument handling to enable them.
    - Swapped the mouse and keyboard listeners so that the keyboard listener is now the non-blocking listener.
    - Removed the code in Cat_Laser_Turret.ino:loop(), since it was no longer needed.
    	+ The loop function doesn't do anything now, but is required to exist for arduino reasons.
    - Cleaned up some unused variables, output calls, comments, etc.
    - Misc. formatting cleanup.

--- controller.py
+++ controller.py
@@ -1,18 +1,18 @@
-from pynput import mouse, keyboard # Used to get control data from the mouse and keyboard
-import serial # Used to communicate with the arduino
-import struct # Used to pack and unpack binary data
-import time
+from pynput import mouse, keyboard # Used to get control data from the mouse and keyboard.
+import serial  # Used to communicate with the arduino.
+import struct  # Used to pack and unpack binary data.
 
 """
-TODO: 
+TODO:
    Add error checking
    Add serial monitoring (print output sent from device)
    Create GUI window and have controls be obtain via the mouse coords w.r.t. the window's bounds
    Hook up a camera to the turret and get the feed to the GUI
    Add controlling over bluetooth connection
-   Automatic detection of the proper COM port
+   Automatic detection of the proper COM port - add constants file or something
+   Add sending of control bytes to allow toggling of flags via commands from the controller script.
 
-   Structure this source code file better (once the basic writing of control data functionality is implemented and BEFORE implementing the GUI stuff)
+   Structure this source code file better (AFTER the basic writing of control data functionality is implemented and BEFORE implementing the GUI stuff)
 """
 
 # Output that the controller script is started.
@@ -30,8 +30,9 @@ print_coords = False # Should mouse coordinates be output on move?
 
 def mouse_coord_to_servo_angle(coord, max_coord):
    # TODO: Add docstring
+
    # Translate the given mouse coordinate (between 0 and max_coord) to an angle between 0 and 180 degrees.
-   # NOTE: This can get real strange and get mouse coordinates outside of the screen bounds if you swipe the mouse quick enough.
+   # FIXME: This can get real strange and get mouse coordinates outside of the screen bounds if you swipe the mouse quick enough.
    # This won't be an issue once we move to using a GUI/window versus the entire monitor to track mouse movement.
 
    return round((coord / max_coord) * 180)
@@ -40,9 +41,9 @@ def on_press(key):
    # TODO: Add docstring
 
    try:
-      print(f'key pressed {key}')
+      print(f'DEBUG: key pressed {key}')
    except AttributeError:
-      print(f'special key pressed {key}')
+      print(f'DEBUG: special key pressed {key}')
 
 def on_release(key):
    # TODO: Add docstring
@@ -58,71 +59,64 @@ def on_release(key):
 
    # Get input and output buffer information.
    if key == keyboard.KeyCode().from_char('b'):
-      print(f'in-buffer bytes: {ser.in_waiting}\nout-buffer bytes: {ser.out_waiting}')
+      print(f'DEBUG: in-buffer bytes: {ser.in_waiting}\nout-buffer bytes: {ser.out_waiting}')
 
-   # Print all bytes in the input buffer, if any.
    # TODO: Have another thread that prints any data as it comes.
-   # if key == keyboard.KeyCode().from_char('f'):
+   # Print all bytes in the input buffer, if any.
+   if key == keyboard.KeyCode().from_char('f'):
+      if ser.in_waiting > 0:
+         print(ser.read(ser.in_waiting))
 
-      # if ser.in_waiting > 0:
-         # print(ser.read(ser.in_waiting))
-
-   # Place mouse coordinates (currently, just X) in the output buffer.
+   # Toggle flag to place mouse coordinates in the output buffer to the arduino.
    if key == keyboard.Key.space:
-      send_coords = True
+      send_coords = not send_coords
 
-   # Toggle output of mouse coordinates to console window.
+   # Toggle flag to output mouse coordinates to console window.
    if key == keyboard.KeyCode.from_char('t'):
       print_coords = not print_coords
-      
+
 
 def on_move(x, y):
    # TODO: Add docstring
 
+   # Global flags
    global send_coords
    global print_coords
 
-   num_bytes_written = 0
+   num_bytes_written = 0 # DEBUG: The number of bytes written to the arduino.
    byte_pack = bytes() # The 'bytes' object containing the packed coordinates sent to the arduino.
 
+   """ Convert the X and Y coordinates w.r.t to the container (currently the monitor) into angles between 0 and 180 degrees. """
    x_angle = mouse_coord_to_servo_angle(x, SCREEN_X)
    y_angle = mouse_coord_to_servo_angle(y, SCREEN_Y)
 
+   # Output the mouse's X and Y coordinates if flag is enabled.
    if print_coords:
       print(f'({x}, {y}) -> ({x_angle}, {y_angle})')
 
-   # TODO: Move this to be done in the on_click() function.
+   # Send the mouse's X and Y coordinates to the arduino if flag is enabled.
    if send_coords:
-      """
-         TODO: If the newer method works, update this comment.
-         Pack the mouse's current X-coordinate into a byte array where the first byte (bits 15:8) is set to 0,
-         indicating that the value of the second byte (bits 7:0) should be sent to the X-axis servo. The second
-         byte is a signed value representing the requested angle for the X-axis servo (between 0 and 180 degrees).
-      """
 
+      # Pack the converted X/Y coordinate data into a byte array of two unsigned bytes.
       byte_pack = struct.pack('BB', x_angle, y_angle)
-      
+
       # DEBUG: Write some debug output.
+      # TODO: Add runtime arguments/flags to enable/disable debug messages.
       # print(f'X int = {x}; X angle = {x_angle}; packed bytes object = {byte_pack}')
       # print(f'unpacked object = {struct.unpack("cB", byte_pack)}')
 
       # Write the packed bytes to the output buffer.
       num_bytes_written = ser.write(byte_pack)
 
-      # DEBUG: Output the number of bytes written to confirm if the bytes were sent as expected.
+      # Output the number of bytes written to confirm if the bytes were sent as expected.
       if num_bytes_written > 0:
-         print(f"Bytes written: {num_bytes_written}")
+         print(f"DEBUG: Bytes written: {num_bytes_written}")
       else:
-         print("No bytes written")
+         print("DEBUG: No bytes written")
 
-      # time.sleep(0.5)
-
-      # We only want to send one set of coordinates at a time, so clear the "send coords" flag.      
-      # send_coords = False
 
 def on_click(x, y, button, pressed):
-   # TODO: Add docstring
-
+   # TODO: Add docstring or remove if not used in the end.
    pass
 
 # Create a non-blocking keyboard listener and start it.
@@ -133,5 +127,5 @@ key_listener.start()
 with mouse.Listener(on_move=on_move, on_click=on_click) as mouse_listener:
    mouse_listener.join()
 
-
+# Let the user know the controller script has ended.
 print('Laser turret controller ended.')
