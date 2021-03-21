4fbced2767df2ca8d7e26eb69114ff9aa11646df
Chan F. Davis <chandlerfdavis@gmail.com>
2021-03-21 18:01:08 -0500

    Add directory and files for development of the TCP-based controller
    - Added skeleton C source and header files.
    - Added Makefile with simple recipes and targets.
    - Added compiled file types (.a, .o, .exe, etc.) to the .gitignore

---
+++ tcp_controller/cat_laser_server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* Inclusions specific to TCP socket programming. */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+/* TODO: Add docstring. */
+int main(void) {
+   /* TODO: Implement */
+
+   return 0;
+}
